e7d36d65e3d06c81d76b197117e35bbdb18cbe0f
Allahnana <Usmanallahnana@gmail.com>
2022-03-15 17:36:45 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Jumbotron from './components/jumbotron/styles';
+
+
+ export default function App() {
+  return (
+  <div>
+    <Jumbotron/>
+  </div>
+    
+  );
+}
+
+
---
+++ src/components/jumbotron/styles/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Inner } from './jumbotron'
+
+function Jumbotron() {
+  return (
+    <div>
+        <Inner>
+            <p>Hello Again</p>
+        </Inner>
+    </div>
+  )
+}
+
+export default Jumbotron
