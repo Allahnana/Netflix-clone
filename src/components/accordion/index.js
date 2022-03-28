43ecf3a77b1a573f62f086efef4dc27176fe64f8
Allahnana <Usmanallahnana@gmail.com>
2022-03-28 01:08:26 +0100

    Mid night commit

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -54,11 +54,7 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
 
 
 
-// Accordion.Body = function AccordionBody({ children, ...restProps }) {
-//     const {toggleShow} = useContext(ToggleContext)
-     
-//     return  toggleShow ? <Body{...restProps}>{children}</Body> : null;
-// };
+
 
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
     const { toggleShow } = useContext(ToggleContext);
