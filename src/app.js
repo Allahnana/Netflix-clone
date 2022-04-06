33be1d7ebb2fbe9b341499fd0326966afd066d56
Allahnana <Usmanallahnana@gmail.com>
2022-04-06 12:38:02 +0100

    ...

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import {Router, Switch} from 'react-router-dom';
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
-
+import * as ROUTES from './constants/routes';
 
 
 
  export default function App() {
   return (
-   <>
+   <Router>
       <JumbotronContainer/>
       <FaqsContainer/>
       <FooterContainer/>
      
-   </>
+   </Router>
   
     
   );
--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -53,6 +53,8 @@ export const Button = styled.button`
 
       &:hover {
           background: #f40612;
+          // background: #f40641;
+
     }
 
       img {
