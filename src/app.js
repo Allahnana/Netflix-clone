94111742a277ac6e55e8ebc5aa72a0fb1606b5c6
Allahnana <Usmanallahnana@gmail.com>
2022-04-07 10:13:11 +0100

    white screen error

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Switch} from 'react-router-dom';
+import {BrowserRouter as Router,  Route, Switch} from 'react-router-dom';
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
@@ -9,10 +9,17 @@ import * as ROUTES from './constants/routes';
 
  export default function App() {
   return (
+
+   
    <Router>
-      <JumbotronContainer/>
-      <FaqsContainer/>
-      <FooterContainer/>
+      <h1> This white screen error na waa ooo</h1>
+
+      <Route exact path = {ROUTES.HOME}>
+         <JumbotronContainer/>
+         <FaqsContainer/>
+         <FooterContainer/>
+      </Route>
+     
      
    </Router>
   
