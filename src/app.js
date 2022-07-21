5d1548b93d4227ec65569f5a73979b7ad9a556fb
Allahnana <usmanallahnana@gmail.com>
2022-07-20 23:35:58 -0700

    Fixing a bug

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Switch,   Route} from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect} from './helpers/routes';
 
 
 
